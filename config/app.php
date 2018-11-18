af24c71ce9e4bb4128beb521b1c71b0b92a75c5d
Mike <mike@tvstuff.com>
2018-11-18 08:37:27 -0600

    composer files
    composer files installed

--- config/app.php
+++ config/app.php
@@ -224,6 +224,12 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+
     ],
 
 ];
